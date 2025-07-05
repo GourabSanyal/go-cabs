c4f1df42a477b5022d460d387a7a41da1f4be82b
ronit-ghosh <ronitghosh06@gmail.com>
2025-07-06 04:17:33 +0530

    feat: add community stack navigator

---
+++ src/routes/Community.Routes.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Community from '../modules/main/Community';
+
+const CommunityStack = createNativeStackNavigator();
+
+const CommunityNavigator = () => {
+  return (
+    <CommunityStack.Navigator
+      screenOptions={{
+        headerShown: false,
+        animation: 'none',
+      }}>
+      <CommunityStack.Screen name="Community" component={Community} />
+    </CommunityStack.Navigator>
+  );
+};
+
+export default CommunityNavigator;
